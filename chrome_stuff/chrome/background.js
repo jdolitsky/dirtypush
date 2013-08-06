9689812644fbe48145abcb88dec80c1d19925ae6
Josh Dolitsky <jdolitsky@mediatemple.net>
2013-08-05 18:34:17 -0700

    LIVE UNLOAD LIVES

---
+++ chrome_stuff/chrome/background.js
@@ -0,0 +1,20 @@
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    console.log(request);
+
+    var method = request.method;
+    if (method == 'saveChange') {
+        chrome.tabs.get(request.tab, function(tab){
+            console.log(tab);
+            $.ajax({   
+                type: 'POST',
+                cache: false,  
+                url: tab.url+'unload.css', 
+                data: {content: request.content},  
+                success: function(data){
+                                          
+                }   
+            }); 
+        });
+    }
+
+});
