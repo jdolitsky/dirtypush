b55a60b1b6fa8e623760c20d8ba311a994e316bc
Josh Dolitsky <jdolitsky@gmail.com>
2013-08-11 11:21:59 -0700

    clean up

--- chrome_stuff/chrome/devtools.js
+++
@@ -1,3 +0,0 @@
-chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(event, content) {
-    chrome.extension.sendRequest({method: 'saveChange', tab: chrome.devtools.inspectedWindow.tabId, content: content});
-});
